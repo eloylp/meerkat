081e9de6b991f413c333f58b99679ec8b72d3077
eloylp <eloylpdev@gmail.com>
2019-06-30 23:05:34 +0200

    Added first working sample
    This is not intended for production use, but its a first approach.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-sentinel
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+	"time"
+)
+
+var images chan []byte
+
+func main() {
+
+	var cameraUrl string
+	flag.StringVar(&cameraUrl, "u", "", "Pass the camera url")
+	var interval int
+	flag.IntVar(&interval, "i", 1, "Pass the camera interval")
+	var listenAddress string
+	flag.StringVar(&listenAddress, "l", "0.0.0.0:3000", "Pass the http server for serving results")
+	flag.Parse()
+	images = make(chan []byte, 10)
+	go func() {
+		for {
+			time.Sleep(time.Duration(interval) * time.Second)
+			resp, err := http.Get(cameraUrl)
+			if err != nil {
+				log.Fatal(err)
+			}
+			jpeg, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if err := resp.Body.Close(); err != nil {
+				log.Fatal(err)
+			}
+			images <- jpeg
+		}
+	}()
+	h := http.NewServeMux()
+	h.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+
+		mimeWriter := multipart.NewWriter(w)
+		contentType := fmt.Sprintf("multipart/x-mixed-replace;boundary=%s", mimeWriter.Boundary())
+		w.Header().Add("Content-Type", contentType)
+
+		for image := range images {
+
+			partHeader := make(textproto.MIMEHeader)
+			partHeader.Add("Content-Type", "image/jpeg")
+
+			partWriter, partErr := mimeWriter.CreatePart(partHeader)
+			if nil != partErr {
+				log.Fatal(partErr.Error())
+			}
+
+			if _, writeErr := partWriter.Write(image); nil != writeErr {
+				log.Fatal(writeErr.Error())
+			}
+		}
+	})
+	h.HandleFunc("/view", func(w http.ResponseWriter, r *http.Request) {
+		r.Header.Add("Content-type", "text/html")
+		html := `<!DOCTYPE html><html><body><img src="/data"></body></html>`
+		_, _ = w.Write([]byte(html))
+	})
+	if err := http.ListenAndServe(listenAddress, h); err != nil {
+		log.Fatal(err)
+	}
+}
