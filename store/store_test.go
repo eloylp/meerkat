f8a00f7d7dd2bc170bf84c8d28b3455783ae8d2f
eloylp <eloylpdev@gmail.com>
2019-09-08 15:23:52 +0200

    Adds cache utilization for new http clients
    Now new clients will be satisfied early with the las frames (in order)
    of the store. Now they will not have to wait to the next data pull.
    This in consequence also fix some false positives in tests related
    with the reset function.

--- store/store_test.go
+++ store/store_test.go
@@ -30,9 +30,12 @@ func TestTimeLineStore_Unsubscribe(t *testing.T) {
 	if subscribersNumResult != expectedSubscribers {
 		t.Errorf("Expected subscribers after unsubscribe is %v got %v", expectedSubscribers, subscribersNumResult)
 	}
-	_, ok := <-ch2
-	if ok {
-		t.Error("Channel is not closed after unsubscribe")
+	var count int
+	for range ch2 {
+		count++
+	}
+	if count != 3 {
+		t.Errorf("Exhausted channel must have last three elements, consumed %v", count)
 	}
 }
 
@@ -51,16 +54,42 @@ func TestTimeLineStore_Unsubscribe_NotFound(t *testing.T) {
 	}
 }
 
+func TestTimeLineStore_Reset(t *testing.T) {
+	s := populatedTimeLineStore(t)
+	s.Reset()
+	listenCh, _ := s.Subscribe()
+	if err := s.AddItem(bytes.NewReader([]byte("dd"))); err != nil {
+		t.Fatal(err)
+	}
+	s.Reset()
+	var count int
+	for item := range listenCh {
+		count++
+		item, err := ioutil.ReadAll(item)
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := "dd"
+		if expected != string(item) {
+			t.Fatalf("Expected nil values after reset")
+		}
+	}
+	if count != 1 {
+		t.Fatal("Only one element was expected in channel")
+	}
+}
 func TestTimeLineStore(t *testing.T) {
 
 	s := populatedTimeLineStore(t)
 	listenCh, _ := s.Subscribe()
 
 	s.Reset()
-	var count uint
-	count++
+	var dataCount uint
+	var itemCount uint
+	dataCount++
 	for item := range listenCh {
-		expected := "d" + fmt.Sprint(count)
+		itemCount++
+		expected := "d" + fmt.Sprint(dataCount)
 		item, err := ioutil.ReadAll(item)
 		if err != nil {
 			t.Fatal(err)
@@ -69,9 +98,11 @@ func TestTimeLineStore(t *testing.T) {
 		if expected != got {
 			t.Fatalf("Error listening subscribed frames. Expected frame was %s but got %s", expected, got)
 		}
-		count++
+		dataCount++
 	}
-
+	if itemCount != 3 {
+		t.Fatal("Not elements in channel !!")
+	}
 }
 
 func populatedTimeLineStore(t *testing.T) *timeLineStore {
