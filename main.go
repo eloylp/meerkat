e296417b6ed9407d7aed30e798fd17c9089c3326
eloylp <eloylpdev@gmail.com>
2019-09-08 17:59:58 +0200

    Adds version build to binary

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/eloylp/meerkat/app"
 	"github.com/eloylp/meerkat/config"
 	"log"
 )
 
+var version string
+
 func main() {
+	fmt.Println(fmt.Sprintf("Meerkat %s", version))
 	cfg := config.C()
 	d := app.NewApp(cfg)
 	if err := d.Start(); err != nil {
