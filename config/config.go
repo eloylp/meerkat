8360db2ca91c5f8785d26864254efb3c9c395784
eloylp <eloylpdev@gmail.com>
2020-04-12 17:00:40 +0200

    Remove global unnecessary global state use

--- config/config.go
+++ config/config.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"os"
 	"strings"
-	"sync"
 )
 
 type Config struct {
@@ -13,24 +12,20 @@ type Config struct {
 	HTTPListenAddress string
 }
 
-var once sync.Once
-var cfg Config
 var validators []validator
 
 func C() Config {
-	once.Do(func() {
-		cfg = Config{}
-		var resources string
-		flag.StringVar(&resources, "u", "", "The comma separated URLS to recover frames from")
-		flag.IntVar(&cfg.PollInterval, "i", 1, "The interval to fill the frame buffer")
-		flag.StringVar(&cfg.HTTPListenAddress, "l", "0.0.0.0:3000", "Pass the http server listen address for serving results")
-		flag.Parse()
-		cfg.Resources = parseResources(resources)
-		if err := Validate(cfg); err != nil {
-			flag.PrintDefaults()
-			os.Exit(1)
-		}
-	})
+	cfg := Config{}
+	var resources string
+	flag.StringVar(&resources, "u", "", "The comma separated URLS to recover frames from")
+	flag.IntVar(&cfg.PollInterval, "i", 1, "The interval to fill the frame buffer")
+	flag.StringVar(&cfg.HTTPListenAddress, "l", "0.0.0.0:3000", "Pass the http server listen address for serving results")
+	flag.Parse()
+	cfg.Resources = parseResources(resources)
+	if err := Validate(cfg); err != nil {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 	return cfg
 }
 
