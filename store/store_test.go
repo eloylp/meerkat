5a80313d6c4941896bb5d9a3a58da1fe2d865d2e
eloylp <eloylpdev@gmail.com>
2020-05-02 19:52:30 +0200

    Improve store test

--- store/store_test.go
+++ store/store_test.go
@@ -12,7 +12,29 @@ import (
 	"testing"
 )
 
-func TestBufferedStore_Subscribe(t *testing.T) {
+func TestBufferedStore_Subscribe_ElementsAreSentToSubscribers(t *testing.T) {
+	items := 3
+	maxItems := 3
+	maxSubsBuffSize := 10
+	s := populatedBufferedStore(t, items, maxItems, maxSubsBuffSize)
+	ch, _ := s.Subscribe()
+	err := s.AddItem(bytes.NewReader([]byte("d3"))) // Extra (4th) data that is removed by excess
+	assert.NoError(t, err)
+	var chOk bool
+	var itemR io.Reader
+	for i := 0; i < items+1; i++ {
+		itemR, chOk = <-ch
+		item, err := ioutil.ReadAll(itemR)
+		assert.NoError(t, err)
+		want := "d" + fmt.Sprint(i)
+		got := string(item)
+		// We check that all datas are present, even removed by excess
+		assert.Equal(t, want, got, "error listening subscribed frames,wanted frame was %q but got %q", want, got)
+	}
+	assert.True(t, chOk, "channel remains open for future consumes")
+}
+
+func TestBufferedStore_Subscribe_ReturnValues(t *testing.T) {
 	items := 3
 	maxItems := 3
 	maxSubsBuffSize := 10
@@ -48,7 +70,7 @@ func TestBufferedStore_Unsubscribe(t *testing.T) {
 	assert.False(t, ok, "want channel closed after unsubscribe and consumed")
 }
 
-func TestBufferedStore_Unsubscribe_notfound(t *testing.T) {
+func TestBufferedStore_Unsubscribe_NotFound(t *testing.T) {
 	items := 3
 	maxItems := 3
 	maxSubsBuffSize := 10
@@ -65,72 +87,23 @@ func TestBufferedStore_Reset(t *testing.T) {
 	ch, _ := s.Subscribe()
 	err := s.AddItem(bytes.NewReader([]byte("dd")))
 	assert.NoError(t, err)
-
 	s.Reset()
-
 	assert.Equal(t, 0, s.Subscribers(), "no subscribers expected after reset")
 	assert.Equal(t, 0, s.Length(), "no items expected after reset")
 	// Check channel is closed afer concumption
 	<-ch
 	_, ok := <-ch
 	assert.False(t, ok)
-
 }
 
-func TestBufferedStore(t *testing.T) {
+func TestNewBufferedStore_AddItem_OldItemsClear(t *testing.T) {
 	items := 3
 	maxItems := 3
 	maxSubsBuffSize := 10
 	s := populatedBufferedStore(t, items, maxItems, maxSubsBuffSize)
-	listenCh, _ := s.Subscribe()
-
-	s.Reset()
-	var dataCount int
-	var itemCount int
-	for item := range listenCh {
-		itemCount++
-		expected := "d" + fmt.Sprint(dataCount)
-		item, err := ioutil.ReadAll(item)
-		if err != nil {
-			t.Fatal(err)
-		}
-		got := string(item)
-		if expected != got {
-			t.Fatalf("Error listening subscribed frames. Expected frame was %s but got %s", expected, got)
-		}
-		dataCount++
-	}
-	if itemCount != 3 {
-		t.Fatal("Not elements in channel !!")
-	}
-}
-
-func TestNewBufferedStore_OldItemsClear(t *testing.T) {
-	items := 3
-	maxItems := 3
-	maxSubsBuffSize := 10
-	s := populatedBufferedStore(t, items, maxItems, maxSubsBuffSize)
-	subs, _ := s.Subscribe()
-	if err := s.AddItem(bytes.NewReader([]byte("d4"))); err != nil {
-		t.Fatal(err)
-	}
-	expectedSize := 3
-	size := s.Length()
-	if size != 3 {
-		t.Errorf("Expected resultant items is %v but %v obtained", expectedSize, size)
-	}
-	s.Reset()
-	var lastItemR io.Reader
-	for item := range subs {
-		lastItemR = item
-	}
-	data, err := ioutil.ReadAll(lastItemR)
-	if err != nil {
-		t.Fatal(err)
-	}
-	lastItem := string(data)
-	expectedLastItem := "d4"
-	if lastItem != expectedLastItem {
-		t.Errorf("Expected last item is %s but got %s", expectedLastItem, lastItem)
-	}
+	err := s.AddItem(bytes.NewReader([]byte("d4")))
+	assert.NoError(t, err)
+	want := 3
+	got := s.Length()
+	assert.Equal(t, want, got, "want %v resultant items got %v", want, got)
 }
